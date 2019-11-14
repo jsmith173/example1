a0750a7cc5459bb725a2d414c46a1e4c02e51732
Attila Csosz <csosz77@gmail.com>
2019-11-14 10:10:08 +0100

    gpio pin

--- gpio_led.c
+++ gpio_led.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 #define ENABLE_LED_MSG	
-#define GPIOPIN gpio68
+#define LARGE_DELAY
 
 void sig_handler(int signo)
 {
@@ -24,35 +24,40 @@ int main(void) {
         exit(1);
     }
 
-    status = access("/sys/class/gpio/GPIOPIN/value", F_OK );
+    status = access("/sys/class/gpio/gpio68/value", F_OK );
 
     if (status == -1) {
         // file doesn't exist
-        printf("GPIOPIN file doesn't exist\n");
+        printf("GPIO_68 file doesn't exist. Execute \'echo $GPIO > export\' \
+                in /sys/class/gpio as root where $GPIO = 68\n");
         exit(1);
     }
 
-    //Set GPIOPIN as output
-    system("echo out > /sys/class/gpio/GPIOPIN/direction"); 
+    //Set GPIO 68 as output
+    system("echo out > /sys/class/gpio/gpio68/direction"); 
     sleep(1);    
 
     while(1) {
         if (cnt % 2 == 0) {
 		   #ifdef ENABLE_LED_MSG	
            printf("%u) LED --- ON\n", cnt);
-           printf("Echo ON for GPIOPIN\n");
 		   #endif
-           system("echo 1 > /sys/class/gpio/GPIOPIN/value");
+           system("echo 1 > /sys/class/gpio/gpio68/value");
         }
         else {
 		   #ifdef ENABLE_LED_MSG	
            printf("%u) LED --- OFF\n", cnt);
 		   #endif
-           system("echo 0 > /sys/class/gpio/GPIOPIN/value");
+           system("echo 0 > /sys/class/gpio/gpio68/value");
         }
 
         cnt += 1;
+		
+        #ifdef LARGE_DELAY
         sleep(1);
+		#else
+        usleep(5000); // 5 ms
+		#endif
     }
 
     return 0;
